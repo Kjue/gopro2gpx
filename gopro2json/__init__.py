c6afd08e8b3fb61c0f3d5baef92329786dda5826
Mikael Lavi <mikael@wanderfeel.com>
2020-09-14 15:42:59 +0300

    Modify init.

--- gopro2json/__init__.py
+++ gopro2json/__init__.py
@@ -1,1 +1,8 @@
 name = "gopro2json"
+
+from . config import *
+from . ffmpegtools import *
+from . fourCC import *
+from . gpmf import *
+from . gpshelper import *
+from . gopro2json import Build360Points
